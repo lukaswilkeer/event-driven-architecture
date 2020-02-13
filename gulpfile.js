07a4b27a0754cb216fb49b71a2d5936a38174bdc
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2020-02-13 15:09:15 -0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const babel = require('gulp-babel')
+const { src, dest, series } = require('gulp')
+const { exec } = require('child_process')
+
+const build = () => src('src/**/*.js')
+  .pipe(babel())
+  .pipe(dest('dist/src/'))
+
+const copyIndex = () => src('index.js')
+  .pipe(babel())
+  .pipe(dest('dist/'))
+
+const start = () => {
+  const server = exec('node dist/index.js', {
+    cwd: process.cwd(),
+    env: { PORT: 3000, MODE: 'prod'}
+  })
+
+  server.stdout.on('data', (chunk) => console.log(chunk.toString()))
+  server.stderr.on('data', (err) => console.error(err))
+
+  return server
+}
+
+exports.default = series(copyIndex, build, start)
