9551933d6e1d4bcb5daf9a3932a20ac4df90b80d
Lukas W. Santos <lukasc4l@gmail.com>
2021-09-24 09:26:26 -0300

    clean architechture

--- test/api/hello.test.js
+++ test/api/hello.test.js
@@ -6,18 +6,18 @@ import { userToken } from '../__mocks__/tokens'
 import '../../index'
 
 describe('Demonstrate module system', () => {
-	it('should respond with a greeting', (done) => {
-		const client = socketConnect(userToken)
+  it('should respond with a greeting', (done) => {
+    const client = socketConnect(userToken)
 
-		const data = {
-			event: 'api.hello.hello.greeting',
-			name: 'a name'
-		}
+    const data = {
+      event: 'api.hello.greeting',
+      name: 'a name'
+    }
 
-		client.emit('event', data)
-		client.on('message', (message) => {
-			expect(message).to.equals(`You're welcome ${data.name}`)
-			done()
-		})
-	})
+    client.emit('event', data)
+    client.on('message', (message) => {
+      expect(message).to.equals(`You're welcome ${data.name}`)
+      done()
+    })
+  })
 })
